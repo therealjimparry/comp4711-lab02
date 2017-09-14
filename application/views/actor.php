747ef8402caa0fb1531ab48f138ae721e9634835
Stephen Cheng <aw3someone@gmail.com>
2017-09-14 14:52:09 -0700

    Display quotes from /application/model/quotes.php on /application/views/actor.php

--- application/views/actor.php
+++ application/views/actor.php
@@ -3,5 +3,6 @@
                 <a href="/welcome/show/{key}">
                         <img src="/pix/{who}.jpg" title="{who} plays {role}"/>
                 </a>
+                <p>{what}</p>
         </div>
 </div>
